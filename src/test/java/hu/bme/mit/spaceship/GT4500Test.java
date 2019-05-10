dd8651ccf700f321790f222299a00e085d099c09
Hopszi <hopszi95@gmail.com>
2019-05-10 12:56:29 +0200

    Update GT4500Test.java
    Wrong use of mockito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,35 +8,35 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
   
-  private DataAccess mockDA;
+  private TorpedoStore mockTS;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    mockDA = mock(DataAccess.class);
-    this.ship = new GT4500(mockDA);
+    mockTS = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTS);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-    when(mockDA.fire(1)).thenReturn(true);
+    when(mockTS.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    verify(mockDA, times(1)).fire(1);
+    verify(mockTS, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-    when(mockDA.fire(1)).thenReturn(true);
+    when(mockTS.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    verify(mockDA, times(1)).fire(1);
+    verify(mockTS, times(1)).fire(1);
   }
 
 }
