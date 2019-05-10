645e3f7a795c2ce859340fcc46a7a8ffb2ba6b41
Hopszi <hopszi95@gmail.com>
2019-05-10 12:40:39 +0200

    Update GT4500Test.java

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -14,7 +14,7 @@ public class GT4500Test {
   @BeforeEach
   public void init(){
     mockDA = mock(DataAccess.class);
-    this.ship = new GT4500();
+    this.ship = new GT4500(mockDA);
   }
 
   @Test
