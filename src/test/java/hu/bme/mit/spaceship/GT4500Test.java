5ae4b1f8889ead2cbb2019202910cabc5c027966
Hopszi <hopszi95@gmail.com>
2019-05-10 13:38:29 +0200

    Update GT4500Test.java
    Test error

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -167,7 +167,7 @@ public class GT4500Test {
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    verify(mockpTS, times(2)).isEmpty();
+    verify(mockpTS, times(1)).isEmpty();
     verify(mockpTS, times(2)).fire(1);
     
     verify(mocksTS, times(1)).isEmpty();
